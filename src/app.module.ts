645dc6f62446a9d6bdc0dc676b3bab104a2fb2e6
mymacbook <vladimir.vavulin@icloud.com>
2023-10-23 16:47:36 +0300

    feat: add dotenv and health.controller.ts

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
+import { HealthController } from './health.controller';
+
+
+@Module({
+  imports: [],
+  controllers: [HealthController],
+  providers: [AppService],
+})
+export class AppModule {}
