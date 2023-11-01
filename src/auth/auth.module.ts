cd538505031bb4436d586fb8712e05b552e6d916
mymacbook <vladimir.vavulin@icloud.com>
2023-11-01 14:16:58 +0200

    feat:  add auth0.strategy and jwt-auth.guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,29 +1,26 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import {User} from "../entities/user.entity";
-import {AuthController} from "./auth.controller";
-import {AuthService} from "./auth.service";
+import { User } from '../entities/user.entity';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 import * as dotenv from 'dotenv';
-import {Auth} from "../entities/auth.entity";
-import {JwtAuthGuard} from "./jwt-auth.guard";
+import { Auth } from '../entities/auth.entity';
 import { PassportModule } from '@nestjs/passport';
 import { Auth0Strategy } from './auth0.strategy';
-
-
 
 dotenv.config();
 
 @Module({
-  imports: [PassportModule.register({ defaultStrategy: 'auth0' }),
+  imports: [
+    PassportModule.register({ defaultStrategy: 'auth0' }),
     TypeOrmModule.forFeature([User, Auth]),
     JwtModule.register({
       secret: process.env.SECRET_KEY,
       signOptions: { expiresIn: '12h' },
-    })
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService, Auth0Strategy],
-
 })
 export class AuthModule {}
