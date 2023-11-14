1bf2b25c0156481314dc9a2f8b657dc263667e07
mymacbook <vladimir.vavulin@icloud.com>
2023-11-14 14:16:13 +0200

    fix

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -10,29 +10,26 @@ import { PaginatedData } from '../types/interface';
 import { CompanyGuard } from '../auth/guard/company.guard';
 import { Invitation } from '../invitation/entity/invitation.entity';
 
+@UseGuards(AuthGuard('jwt'), CompanyGuard)
 @Controller('companies')
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
 
   @Post()
-  @UseGuards(AuthGuard('jwt'))
   createCompany(@GetUser() user: User, @Body() companyDto: CompanyCreateDto): Promise<Company> {
     return this.companyService.createCompany(user, companyDto);
   }
 
   @Patch(':id')
-  @UseGuards(AuthGuard('jwt'), CompanyGuard)
   updateCompany(@Param('id') id: string, @Body() companyDto: CompanyUpdateDto): Promise<Company> {
     return this.companyService.updateCompany(+id, companyDto);
   }
 
   @Get(':id')
-  @UseGuards(AuthGuard('jwt'))
   getCompanyById(@Param('id') id: string): Promise<Company> {
     return this.companyService.getCompanyById(+id);
   }
   @Get(':id/members')
-  @UseGuards(AuthGuard('jwt'))
   async getCompanyMembers(
     @Param('id') id: string,
     @Query('page') page = 1,
@@ -41,7 +38,6 @@ export class CompanyController {
     return this.companyService.getCompanyMembers(+id, page, limit);
   }
   @Get(':id/invitations')
-  @UseGuards(AuthGuard('jwt'))
   async getCompanyInvitations(
     @Param('id') id: string,
     @Query('page') page = 1,
@@ -51,48 +47,35 @@ export class CompanyController {
   }
 
   @Delete(':id')
-  @UseGuards(AuthGuard('jwt'), CompanyGuard)
   deleteCompany(@Param('id') id: string): Promise<void> {
     return this.companyService.deleteCompany(+id);
   }
 
   @Patch(':id/add-admin/:userId')
-  @UseGuards(AuthGuard('jwt'), CompanyGuard)
-  async addAdminToCompany(
-    @Param('id') companyId: string,
-    @Param('userId') userId: string,
-  ): Promise<Company> {
+  async addAdminToCompany(@Param('id') companyId: string, @Param('userId') userId: string): Promise<Company> {
     return this.companyService.addAdminToCompany(+companyId, +userId);
   }
 
   @Patch(':id/remove-admin/:userId')
-  @UseGuards(AuthGuard('jwt'), CompanyGuard)
-  async removeAdminFromCompany(
-    @Param('id') companyId: string,
-    @Param('userId') userId: string,
-  ): Promise<Company> {
+  async removeAdminFromCompany(@Param('id') companyId: string, @Param('userId') userId: string): Promise<Company> {
     return this.companyService.removeAdminFromCompany(+companyId, +userId);
   }
 
   @Delete(':id/exclude-user/:userId')
-  @UseGuards(AuthGuard('jwt'), CompanyGuard)
   async excludeUserFromCompany(@Param('id') companyId: string, @Param('userId') excludeUserId: string): Promise<void> {
     return this.companyService.excludeUserFromCompany(+excludeUserId, +companyId);
   }
 
   @Delete(':id/leave')
-  @UseGuards(AuthGuard('jwt'))
   async leaveCompany(@GetUser() user: User, @Param('id') companyId: number): Promise<void> {
     return this.companyService.leaveCompany(+user.id, +companyId);
   }
 
   @Get()
-  @UseGuards(AuthGuard('jwt'))
   getAllCompanies(@Query('page') page = 1, @Query('limit') limit = 10): Promise<PaginatedData<Company>> {
     return this.companyService.findAll(+page, +limit);
   }
   @Get(':id/admins')
-  @UseGuards(AuthGuard('jwt'), CompanyGuard)
   async getCompanyAdmins(@Param('id') id: string): Promise<User[]> {
     return this.companyService.getCompanyAdmins(+id);
   }
--- src/invitation/invitation.controller.ts
+++ src/invitation/invitation.controller.ts
@@ -6,43 +6,37 @@ import { PaginatedData } from '../types/interface';
 import { Company } from '../company/entity/company.entity';
 import { GetUser } from '../decorator/getUser.decorator';
 import { User } from '../users/entities/user.entity';
-
+@UseGuards(AuthGuard('jwt'))
 @Controller('invitations')
 export class InvitationController {
   constructor(private readonly invitationService: InvitationService) {}
 
   @Post('/sendInvitation')
-  @UseGuards(AuthGuard('jwt'))
   async sendInvitation(@GetUser() user: User, @Body() invitationDto: InvitationDto): Promise<void> {
     await this.invitationService.sendInvitation(invitationDto, user);
   }
 
   @Post('/sendRequest')
-  @UseGuards(AuthGuard('jwt'))
   async senRequest(@GetUser() user: User, @Body() invitationDto: InvitationDto): Promise<void> {
     await this.invitationService.sendRequest(invitationDto, user);
   }
 
   @Post(':id/accept')
-  @UseGuards(AuthGuard('jwt'))
   async accept(@Param('id') id: string): Promise<void> {
     await this.invitationService.accept(+id);
   }
 
   @Post(':id/reject')
-  @UseGuards(AuthGuard('jwt'))
   async reject(@Param('id') id: string): Promise<void> {
     await this.invitationService.reject(+id);
   }
 
   @Delete(':id')
-  @UseGuards(AuthGuard('jwt'))
   async softDeleteInvitation(@Param('id') id: string): Promise<void> {
     await this.invitationService.softDeleteInvitation(+id);
   }
 
   @Get(':id/invitations')
-  @UseGuards(AuthGuard('jwt'))
   async getInvitationsForUser(
     @Param('id') id: string,
     @Query('page') page = 1,
@@ -51,7 +45,6 @@ export class InvitationController {
     return await this.invitationService.getInvitationsForUser(+id, page, limit);
   }
   @Get(':id/invitations')
-  @UseGuards(AuthGuard('jwt'))
   async getRequestedForUser(
     @Param('id') id: string,
     @Query('page') page = 1,
