29fa55c1b570ddb50f62b9624a027dd59a2919e2
mymacbook <vladimir.vavulin@icloud.com>
2023-12-06 18:45:16 +0200

    tests

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,28 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CacheModule } from '@nestjs/cache-manager';
 import { ConfigService } from '@nestjs/config';
-import { ExportService } from './export.service';
 import { redisStore } from 'cache-manager-redis-yet';
+import { ExportService } from './export.service';
 
 @Module({
   imports: [
     CacheModule.registerAsync({
-      useFactory: async (configService: ConfigService) => {
-        const redisHost = configService.get<string>('REDIS_HOST');
-        const redisPort = configService.get<number>('REDIS_PORT');
-        const redisTtl = configService.get<number>('REDIS_TTL');
-
-        console.log('REDIS_HOST:', redisHost);
-        console.log('REDIS_PORT:', redisPort);
-        console.log('REDIS_TTL:', redisTtl);
-
-        return {
-          store: redisStore,
-          host: redisHost,
-          port: redisPort,
-          ttl: redisTtl,
-        };
-      },
+      useFactory: async (configService: ConfigService) => ({
+        store: await redisStore({
+          url: configService.get<string>('REDIS_URL'),
+          ttl: configService.get<number>('REDIS_TTL'),
+        }),
+      }),
       isGlobal: true,
       inject: [ConfigService],
     }),
